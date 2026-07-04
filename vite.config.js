d601751b0b6aebbda184d143337b909c680f9f59
Menuka Dewsith <menukadewsith@Dewsiths-MacBook-Air.local>
2026-07-04 17:43:34 +0530

    Fix routing and controller issues

--- vite.config.js
+++ vite.config.js
@@ -21,14 +21,4 @@ export default defineConfig({
         },
     },
 
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.jsx',
-            },
-        },
-    },
 });
