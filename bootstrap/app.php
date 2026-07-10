f8aa7072109e96911217d7e64f64de44a885f078
Menuka Dewsith <menukadewsith@Dewsiths-MacBook-Air.local>
2026-07-10 20:36:36 +0530

    Remove EnsureValidSession middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,10 +24,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->web(append: [
-            \App\Http\Middleware\EnsureValidSession::class,
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+    \App\Http\Middleware\HandleInertiaRequests::class,
+    \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+    ]);
 
         // FIX: Role-based redirect for already-authenticated users
         $middleware->redirectUsersTo(function (Request $request) {
